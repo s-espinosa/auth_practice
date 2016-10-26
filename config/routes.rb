d7060a210d6cab04c800ef3e379d16682f0da7d2
Sal Espinosa <sespinos@gmail.com>
2016-10-25 21:04:59 -0600

    Add ability to login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
+
+  get    '/login',  to: 'sessions#new',     as: 'login'
+  post   '/login',  to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
 end
