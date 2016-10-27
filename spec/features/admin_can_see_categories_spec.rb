8bd2acaaaaf9233bc28ce2df3ae112d7b040176e
Sal Espinosa <sespinos@gmail.com>
2016-10-26 22:02:43 -0600

    Add tests for authorization

---
+++ spec/features/admin_can_see_categories_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe 'User visits the admin categories page' do
+  context 'as an admin' do
+    it 'they see Admin Categories' do
+      admin = User.create(name: "sal",
+                          email: "sal@sal.com",
+                          password: "pass",
+                          password_confirmation: "pass",
+                          role: 1
+                         )
+      ApplicationController.any_instance.stubs(:current_user).returns(admin)
+
+      visit admin_categories_path
+      expect(page).to have_content("Admin Categories")
+    end
+  end
+
+  context 'as a default user' do
+    it 'they are shown a 404' do
+      user = User.create(name: "Sal",
+                         email: "sal@sal.com",
+                         password: "pass",
+                         password_confirmation: "pass"
+                        )
+
+      ApplicationController.any_instance.stubs(:current_user).returns(user)
+      visit admin_castegories_path
+      expect(page).to_not have_content("Admin Categories")
+      expect(page).to have_content("The page you were looking for doesn't exist")
+    end
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe User do
+  describe '#role' do
+    it 'can return admin' do
+      user = User.create(name: 'Sal',
+                         email: 'sal@sal.com',
+                         password: 'pass',
+                         password_confirmation: 'pass',
+                         role: 1
+                        )
+
+      expect(user.role).to eq("admin")
+      expect(user.admin?).to be_true
+    end
+
+    it 'can return default' do
+      user = User.create(name: 'Penelope',
+                         email: 'pen@pen.com',
+                         password: 'pass',
+                         password_confirmation: 'pass'
+                        )
+
+      expect(user.role).to eq("default")
+      expect(user.default?).to be_true
+    end
+  end
+end
