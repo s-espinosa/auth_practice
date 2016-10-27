ed2867291216484b642ec9ba4fa450f005620a3d
Sal Espinosa <sespinos@gmail.com>
2016-10-26 22:18:12 -0600

    Clean tests

--- spec/features/admin_can_see_categories_spec.rb
+++ spec/features/admin_can_see_categories_spec.rb
@@ -25,7 +25,7 @@ describe 'User visits the admin categories page' do
                         )
 
       ApplicationController.any_instance.stubs(:current_user).returns(user)
-      visit admin_castegories_path
+      visit admin_categories_path
       expect(page).to_not have_content("Admin Categories")
       expect(page).to have_content("The page you were looking for doesn't exist")
     end
